a9b329557777f51e9102edef374b5cec01926510
zylyehuo <zyl_0606@163.com>
2025-08-13 00:08:41 +0800

    code update

---
+++ build/slam_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yehuo/ros_test_ws/src/slam_pkg
+# Build directory: /home/yehuo/ros_test_ws/build/slam_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
